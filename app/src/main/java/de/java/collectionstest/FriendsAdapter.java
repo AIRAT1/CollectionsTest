8da2d139b15413bae705bfdd15138b258102e912
Ayrat <ayrat1@mail.ru>
2015-11-18 22:44:41 +0300

    final lesson state

--- app/src/main/java/de/java/collectionstest/FriendsAdapter.java
+++ app/src/main/java/de/java/collectionstest/FriendsAdapter.java
@@ -1,4 +1,65 @@
 package de.java.collectionstest;
 
-public class FriendsAdapter {
+import android.content.Context;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class FriendsAdapter extends BaseAdapter {
+    private List<Person> friends;
+    private Context context;
+
+    public FriendsAdapter(List<Person> friends, Context context) {
+        this.friends = friends;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return friends.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return friends.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Person p = friends.get(position);
+        View friendView = convertView;
+        if (friendView == null) {
+            friendView = LayoutInflater.from(context).inflate(R.layout.listitem_friend, null);
+        }
+        ImageView ivAvatar = (ImageView) friendView.findViewById(R.id.avatar);
+        if (p.isAvatarExist()) {
+            ivAvatar.setImageResource(R.drawable.friend1);
+        } else {
+            ivAvatar.setImageResource(R.drawable.no_avatar);
+        }
+        TextView tvName = (TextView) friendView.findViewById(R.id.name);
+        tvName.setText(p.getName());
+        ImageView ivFavourite = (ImageView) friendView.findViewById(R.id.favourite);
+        if (p.isFavourite()) {
+            ivFavourite.setVisibility(View.VISIBLE);
+        } else {
+            ivFavourite.setVisibility(View.GONE);
+        }
+        TextView tvContacts = (TextView) friendView.findViewById(R.id.email);
+        tvContacts.setText(p.getTelephone() + " " + p.getEmail());
+        TextView tvBirthday = (TextView) friendView.findViewById(R.id.birthday);
+        tvBirthday.setText(DateFormat.format("ДР: dd.MM.yyyy", p.getBirth()));
+        return friendView;
+    }
 }
--- app/src/main/java/de/java/collectionstest/MainActivity.java
+++ app/src/main/java/de/java/collectionstest/MainActivity.java
@@ -2,54 +2,30 @@ package de.java.collectionstest;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.text.format.DateFormat;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
-    private ArrayList<Person> friends;
+    private List<Person> friends;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.lv_friends);
         createFriends();
         fillList();
     }
-
     private void fillList() {
-        LinearLayout llFriends = (LinearLayout) findViewById(R.id.ll_friend);
-        for (Person p : friends) {
-            View friendView = getLayoutInflater().inflate(R.layout.listitem_friend, null);
-            ImageView ivAvatar = (ImageView)friendView.findViewById(R.id.avatar);
-            if (p.isAvatarExist()) {
-                ivAvatar.setImageResource(R.drawable.friend1);
-            }else {
-                ivAvatar.setImageResource(R.drawable.no_avatar);
-            }
-            TextView tvName = (TextView) friendView.findViewById(R.id.name);
-            tvName.setText(p.getName());
-            ImageView ivFavourite = (ImageView) friendView.findViewById(R.id.favourite);
-            if (p.isFavourite()) {
-                ivFavourite.setVisibility(View.VISIBLE);
-            }else {
-                ivFavourite.setVisibility(View.GONE);
-            }
-            TextView tvContacts = (TextView) friendView.findViewById(R.id.email);
-            tvContacts.setText(p.getTelephone() + " " + p.getEmail());
-            TextView tvBirthday = (TextView) friendView.findViewById(R.id.birthday);
-            tvBirthday.setText(DateFormat.format("ДР: dd.MM.yyyy", p.getBirth()));
-            llFriends.addView(friendView);
-        }
+        listView.setAdapter(new FriendsAdapter(friends, this));
     }
 
     private void createFriends() {
         friends = new ArrayList<>();
-        for (int i = 0; i < 200; i++) {
+        for (int i = 0; i < 20000; i++) {
             Person p = new Person();
             p.setName("Иван Петрович № " + i);
             p.setBirth(System.currentTimeMillis() - 1000000000L * i);
